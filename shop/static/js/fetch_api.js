19b273612ac67c7b1cf7a68d40da1822c3fd23e4
Ilya Homyakov <36329586+IlyaHomyakov@users.noreply.github.com>
2021-12-03 18:27:36 +0300

    updated

--- shop/static/js/fetch_api.js
+++ shop/static/js/fetch_api.js
@@ -2,16 +2,46 @@ confirmButton = document.getElementById('confirm-button')
 const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 const order = JSON.parse(localStorage.getItem('cart'))
 
+let customerNameInput = document.getElementById('customer-name-input')
+let customerPhoneInput = document.getElementById('customer-phone-input')
 
-confirmButton.onclick = e =>
+customerNameInput.onchange = e => {
+    customerNameInput.classList.remove('customer-data-input-invalid')
+}
+customerPhoneInput.onchange = e => {
+    customerPhoneInput.classList.remove('customer-data-input-invalid')
+}
 
-    fetch('http://127.0.0.1:8000/confirm-order/', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken
-        },
-        body: JSON.stringify(order)
-    }).then(r => console.log(r))
+confirmButton.onclick = e => {
 
+    if (customerNameInput.value === '') {
+        customerNameInput.placeholder = 'Введите ФИО'
+        customerNameInput.classList.add('customer-data-input-invalid')
+    }
+    if (customerPhoneInput.value === '') {
+        customerPhoneInput.placeholder = 'Введите номер телефона'
+        customerPhoneInput.classList.add('customer-data-input-invalid')
+    } else {
+
+        let orderObject = {
+            customer: {
+                name: customerNameInput.value,
+                phone: customerPhoneInput.value
+            },
+            cart: order
+        }
+
+
+        fetch('http://127.0.0.1:8000/confirm-order/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify(orderObject)
+        }).then(r => console.log(r))
+    }
+
+
+}
